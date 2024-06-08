8c7e0952342a5d74e133b4423107249bce80324f
roselyn <roselynh100@gmail.com>
2024-06-08 00:48:31 -0400

    LETTER IS GOIGN CRAZYYY (animations)

--- src/routes/Letter.tsx
+++ src/routes/Letter.tsx
@@ -8,7 +8,7 @@ import {
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Note } from "../assets";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Letter = () => {
   const db = getFirestore();
@@ -49,17 +49,50 @@ const Section = styled.div`
   align-items: center;
 `;
 
-// TODO: cool animation for this heading
-const LetterHeading = styled.h2`
-  margin: 0;
-  padding: 0;
+const fadeInHorizontal = keyframes`
+  0% {
+    -webkit-mask-size: 0%;
+    -webkit-mask-image: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 1) 90%,
+      rgba(0, 0, 0, 0)
+    );
+  }
+  100% {
+    -webkit-mask-size: 100%;
+    -webkit-mask-image: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 1) 90%,
+      rgba(0, 0, 0, 0)
+    );
+  }
+`;
+
+const LetterHeading = styled.h1`
+  margin: 0 0 24px 0;
   cursor: default;
+  color: white;
+  text-shadow: black 1px 0 10px;
+  animation: ${fadeInHorizontal} 1.5s ease-in-out;
+  -webkit-mask-repeat: no-repeat;
+`;
+
+const floatUpAnimation = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(20vh);
+  }
+
+  100% {
+    opacity: 1;
+  }
 `;
 
 const NoteWrapper = styled.div`
   display: grid;
   grid-template: 1fr;
   height: 70vh;
+  animation: ${floatUpAnimation} 1.5s ease;
 `;
 
 const NoteImg = styled.img`
